d3bb0a4e35d2d0b9ed72e93cecf41c9f1bdb19e7
RUBIS <id@rubis.snu.ac.kr>
2024-11-08 11:46:16 +0900

    magic number 1.7

--- src/components/recorder/AudioRecorder.jsx
+++ src/components/recorder/AudioRecorder.jsx
@@ -833,7 +833,7 @@ const AudioRecorder = ({
   const voiceStopTimerRef = useRef(null);
 
   const VOICE_START_DEBOUNCE = 50;
-  const VOICE_STOP_DEBOUNCE = 2200;
+  const VOICE_STOP_DEBOUNCE = 1700;
 
   const isUploadingRef = useRef(false);
 
